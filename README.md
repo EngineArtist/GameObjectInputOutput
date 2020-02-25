57cf1b0719a7e0b93e2f28189dd1628f18317ef6
Miika Vihersaari <miika.vihersaari@gmail.com>
2020-02-25 16:13:17 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# GameObject InputOutput
+
+A Component for unified signalling between Unity GameObjects.
+
+## Requirements
+
+- Unity 2019.3 or newer
+- com.engineartist.gameobject-attributes 0.1.0
+
+## Installing
+
+Installation happens through Unity's Package Manager. Open the Package Manager -window, click the plus-icon in the top-left corner and select 'Add package from git URL'. In the field, type:
+https://github.com/EngineArtist/GameObjectInputOutput.git
+Now the package should show up in the list of packages and be installed automatically.
+
+## Usage
+
+Once the package has been installed, a new GameObject Component called InputOutput should now be available. Select a GameObject you want to be able to send and receive signals to and from other GameObjects. Then click the Add Component -button in the Inspector. Write 'InputOutput' in the search field and select the InputOutput -component. You can now add inputs and outputs to your GameObject.
+
+## Author
+
+Miika Vihersaari <miika.vihersaari@gmail.com>
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
+## Acknowledgements
+
+- Thanks to Unity for making a great engine and editor!
---
+++ Runtime/InputOutput.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Reflection;
+
+
+[Serializable]
+public struct OutputSlot {
+    public string name;
+    public GameObject target;
+    public string input;
+    [SerializeReference] public IAttribute[] args;
+}
+
+
+[Serializable]
+public struct OutputBind {
+    public GameObject self;
+    public string output;
+}
+
+
+[Serializable]
+public struct InputSlot {
+    public string name;
+    public InputBind inputBind;
+}
+
+
+[Serializable]
+public class InputBind {
+    public string componentName;
+    public string methodName;
+    //public string[] argTypeNames;
+
+    private MethodInfo _methodInfo;
+    private Component _component;
+
+    public void Call(GameObject target, string signalName, IAttribute[] args) {
+        if (methodName == null || methodName == "") {
+            return;
+        }
+        if (_methodInfo != null) {
+            _methodInfo.Invoke(_component, null);
+            return;
+        }
+        _component = target.GetComponent(componentName);
+        if (_component == null) {
+            Debug.LogWarning("Cannot process signal '" + signalName + "': GameObject '" + target.name + "' doesn't have Component '" + componentName + "'");
+            return;
+        }
+        var compType = _component.GetType();
+        //Type[] argTypes = new Type[argTypeNames.Length];
+        //for (int i = 0; i < argTypes.Length; ++i) {
+        //    argTypes[i] = Type.GetType(argTypeNames[i], false, false);
+        //}
+        _methodInfo = compType.GetMethod(methodName, new Type[] {});
+        if (_methodInfo == null) {
+            Debug.LogWarning("Cannot process signal '" + signalName + "': GameObject '" + target.name + "' with Component '" + componentName + "' doesn't have method '" + methodName + "' with the required arguments");
+            return;
+        }
+        _methodInfo.Invoke(_component, null);
+    }
+}
+
+
+public class InputOutput: MonoBehaviour {
+    public List<OutputSlot> outputs;
+    public List<InputSlot> inputs;
+}
+
+
+public static class InputOutputExtensions {
+    public static void SendSignal(this GameObject gobj, string name) {
+        var sign = gobj.GetComponent<InputOutput>();
+        if (sign != null) {
+            for (int i = 0; i < sign.outputs.Count; ++i) {
+                if (sign.outputs[i].name == name && sign.outputs[i].target != null) {
+                    sign.outputs[i].target.ReceiveSignal(
+                        sign.outputs[i].input,
+                        sign.outputs[i].args
+                    );
+                    return;
+                }
+            }
+        }
+    }
+
+    public static void ReceiveSignal(this GameObject gobj, string name, IAttribute[] args) {
+        var sign = gobj.GetComponent<InputOutput>();
+        if (sign != null) {
+            for (int i = 0; i < sign.inputs.Count; ++i) {
+                if (sign.inputs[i].name == name) {
+                    sign.inputs[i].inputBind.Call(gobj, name, args);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+
+[CustomPropertyDrawer(typeof(OutputSlot))]
+public class OutputSlotDrawer: PropertyDrawer {
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+        //property.FindPropertyRelative("targets");
+        return property.isExpanded ? 54f: 16f;
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        EditorGUI.BeginProperty(position, label, property);
+        var name = property.FindPropertyRelative("name");
+        var foldoutLabel = name.stringValue;
+        if (foldoutLabel == null || foldoutLabel == "") foldoutLabel = "Unnamed output";
+        property.isExpanded = EditorGUI.Foldout(new Rect(position.x, position.y, property.isExpanded ? 19f: position.width, 16f), property.isExpanded, property.isExpanded ? "": foldoutLabel, true, EditorStyles.foldout);
+        if (property.isExpanded) {
+            var target = property.FindPropertyRelative("target");
+            var input = property.FindPropertyRelative("input");
+            name.stringValue = EditorGUI.TextField(new Rect(position.x, position.y, position.width, 16f), name.stringValue);
+            EditorGUI.PropertyField(new Rect(position.x, position.y + 16f, position.width, 16f), target);
+            var targetGobj = (GameObject)target.objectReferenceValue;
+            if (targetGobj != null) {
+                var sign = targetGobj.GetComponent<InputOutput>();
+                if (sign != null && sign.inputs != null && sign.inputs.Count > 0) {
+                    string[] inputNames = new string[sign.inputs.Count];
+                    for (int i = 0; i < inputNames.Length; ++i) {
+                        inputNames[i] = sign.inputs[i].name;
+                    }
+                    int sel = Array.FindIndex(inputNames, (string s) => s == input.stringValue);
+                    if (sel < 0) sel = 0;
+                    input.stringValue = inputNames[EditorGUI.Popup(new Rect(position.x, position.y + 32f, position.width, 16f), "Target input", sel, inputNames)];
+                }
+                else {
+                    input.stringValue = "";
+                }
+            }
+        }
+        EditorGUI.EndProperty();
+    }
+}
+
+
+[CustomPropertyDrawer(typeof(InputSlot))]
+public class InputSlotDrawer: PropertyDrawer {
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+        return property.isExpanded ? 56f: 16f;
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        EditorGUI.BeginProperty(position, label, property);
+        var name = property.FindPropertyRelative("name");
+        var foldoutLabel = name.stringValue;
+        if (foldoutLabel == null || foldoutLabel == "") foldoutLabel = "Unnamed input";
+        property.isExpanded = EditorGUI.Foldout(new Rect(position.x, position.y, property.isExpanded ? 19f: position.width, 16f), property.isExpanded, property.isExpanded ? "": foldoutLabel, true, EditorStyles.foldout);
+        if (property.isExpanded) {
+            var inputBind = property.FindPropertyRelative("inputBind");
+            var componentName = inputBind.FindPropertyRelative("componentName");
+            var gobj = Selection.gameObjects[0];
+            var comps = gobj.GetComponents<Component>();
+            string[] compNames = new string[comps.Length];
+            for (int i = 0; i < comps.Length; ++i) {
+                compNames[i] = comps[i].GetType().Name;
+            }
+            var compIndex = Array.IndexOf(compNames, componentName.stringValue);
+            if (compIndex < 0) compIndex = 0;
+            var methodName = inputBind.FindPropertyRelative("methodName");
+            name.stringValue = EditorGUI.TextField(new Rect(position.x, position.y, position.width, 16f), name.stringValue);
+            componentName.stringValue = compNames[EditorGUI.Popup(new Rect(position.x, position.y + 16f, position.width, 16f), "Component", compIndex, compNames)];
+            var methods = gobj.GetComponent(componentName.stringValue).GetType().GetMethods(BindingFlags.Instance|BindingFlags.Public);
+            List<string> methodNames = new List<string>();
+            for (int i = 0; i < methods.Length; ++i) {
+                if (methods[i].GetParameters().Length == 0) {
+                    methodNames.Add(methods[i].Name);
+                }
+            }
+            var methodIndex = methodNames.IndexOf(methodName.stringValue);
+            if (methodIndex < 0) methodIndex = 0;
+            methodName.stringValue = methodNames[EditorGUI.Popup(new Rect(position.x, position.y + 34f, position.width, 16f), "Method", methodIndex, methodNames.ToArray())];
+        }
+        EditorGUI.EndProperty();
+    }
+}
